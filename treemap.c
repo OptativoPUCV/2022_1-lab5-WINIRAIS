08f376f021d9aa4af4c56a452cbb69fc6d841842
Your Name <you@example.com>
2022-04-23 00:51:44 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -75,10 +75,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     int resultado = tree->lower_than(key,aux->pair->key);
     while (aux != NULL){ 
         if((is_equal(tree,key,aux->pair->key) == 1)){ 
-            tree->current->left = aux->left;
-            tree->current->right = aux->right;
-            tree->current->parent = aux->parent;
-            tree->current->pair = aux->pair;
+            tree->root = aux;
             return aux->pair;
         }
         if(resultado == 1)
